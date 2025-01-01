8254e8726502e29fc73fc5ef745496995df093ed
ju214425 <ju214425@naver.com>
2025-01-01 14:58:12 +0900

    Chore : db init 파일 수정
    SQLAlchemy에서 자동으로 테이블 수정하도록 init 파일 수정

---
+++ back/app/__init__.py
@@ -0,0 +1,6 @@
+from app.models.answer import Answer
+from app.models.exam import Exam
+from app.models.question import Question
+from app.models.subject import Subject
+from app.models.user_exam import UserExam
+from app.models.user import User
